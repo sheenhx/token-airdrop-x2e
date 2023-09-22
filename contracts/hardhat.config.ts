5c93ed732a02944d06cf081bca25a0fcf1effac1
Sheen <9674168+sheenhx@users.noreply.github.com>
2023-09-22 13:58:19 +0200

    fix: Update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -9,10 +9,14 @@ const config: HardhatUserConfig = {
     anvil: {
       url: "http://localhost:8545",
     },
-    mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/UMRGB-eJSGwtjt3_dzIuUOZWp0hVkYLT",
-      accounts: ["6ae4bf9ec354bb7770407fef831151d5857d731a8bb98ce0b1d215861337e8e4"]
-    }
+    wannsee: {
+            url: "http://207.246.99.8:8545",
+            chainId: 5167003,
+            // accounts: real_accounts,
+            accounts: ["6ae4bf9ec354bb7770407fef831151d5857d731a8bb98ce0b1d215861337e8e4"] as HttpNetworkAccountsUserConfig | undefined,
+            gasPrice: 6000000000000,
+            saveDeployments: true,
+        }
   }
 };
 
